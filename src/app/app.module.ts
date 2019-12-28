70d557eb1a390651b93c40f1e66aa83ad541b956
Damian Tabaczynski <dam.tab@wp.pl>
2019-12-28 12:25:38 +0100

    Add primeng and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    PanelModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MessagesModule,
+    MessageModule,
     AppRoutingModule
   ],
   providers: [],
