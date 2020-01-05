c804adde59f652fce6b7211ab4eb70ce9980723e
tgrzesiak <tom.grzes3141@gmail.com>
2020-01-05 16:56:46 +0100

    Added dropdowns with template data

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+  pages = [
+    { name: "Woda", subpages: ["Woda 1", "Woda 2", "Woda 3"] },
+    { name: "Ścieki", subpages: ["Reaktor 1", "Reaktor 2", "Reaktor 3"] },
+    { name: "Anamox", subpages: ["Reaktor 1", "Reaktor 2", "Reaktor 3"] },
+    { name: "Scenariusze", subpages: ["Dostępne scenariusze", "Dodaj nowy scenariusz", "Opcje"] },
+    { name: "Ustawienia", subpages: ["Zmień dane", "Wyloguj"] }
+  ];
 
-  data = ['Woda', 'Ścieki', 'Anamox', 'Scenariusze', 'Ustawienia']
-
-  constructor() { }
-
-  ngOnInit() {
+  onClick(name) {
+    console.log(name);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
