0058507d4c03d8ec90a3d9fa827a2cc61b42cc4d
tgrzesiak <tom.grzes3141@gmail.com>
2020-01-11 20:47:10 +0100

    Requested changes

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-navbar",
+  selector: "sw-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+
   pages = [
     { name: "Woda", subpages: ["Woda 1", "Woda 2", "Woda 3"] },
     { name: "Ścieki", subpages: ["Reaktor 1", "Reaktor 2", "Reaktor 3"] },
@@ -14,11 +15,8 @@ export class NavbarComponent implements OnInit {
     { name: "Ustawienia", subpages: ["Zmień dane", "Wyloguj"] }
   ];
 
-  onClick(name) {
+  onClick(name: string): void {
     console.log(name);
   }
 
-  constructor() {}
-
-  ngOnInit() {}
 }
