da9d99f70806d7fd13c61e8969a52aa08e64851d
dokolyski <dominikkalinowski15@wp.pl>
2020-02-11 00:13:21 +0100

    Authorization works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WaterModule } from './forms/water/water.module';
 import { NavbarModule } from './shared/components/navbar/navbar.module';
 import { ScenarioModule } from './forms/scenario/scenario.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ErrorInterceptor } from './shared/services/error-interceptor/error.interceptor';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -24,7 +21,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
