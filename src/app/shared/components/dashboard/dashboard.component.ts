267c78b4b6afa22842a958ea103abba42ee35535
Damian Tabaczynski <dam.tab@wp.pl>
2020-02-10 22:22:26 +0100

    Refactor code and fix scenario

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,28 +1,28 @@
 import { Component, Input, ContentChildren, QueryList, ChangeDetectorRef } from '@angular/core';
 import { TrackComponent } from './track/track.component';
 
-/* 
+/*
  * <sw-dashboard show="one">
  *  <sw-track name="one">
  *   <sw-track-widget header="header" subheader="subheader">
- *    
- *      ... 
- * 
+ *
+ *      ...
+ *
  *   </sw-track-widget>
  *  </sw-track>
- * </sw-dashboard> 
+ * </sw-dashboard>
  *
  * sw-dashboard - dashboard of subtracks, shows only one at the same time
  * sw-track - set of widgets
  * sw-track-widget - widget on dashboard
- * 
+ *
  * Attributes:
  * a) sw-dashboard
  * show - id of sw-track to be shown
- * 
+ *
  * b) sw-track
  * name - id of sw-track
- * 
+ *
  * c) sw-track-widget
  * header - p-card header
  * subheader - p-card subheader
@@ -35,7 +35,6 @@ import { TrackComponent } from './track/track.component';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  @Input('show') readonly selectedTrack: string;
   @ContentChildren(TrackComponent) readonly tracks: QueryList<TrackComponent>;
 
   constructor(private cdr: ChangeDetectorRef) {}
