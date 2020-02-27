6957af36965d9270fca5e9f782f2e28b6af9cf30
ChairChandler <razzors1998@gmail.com>
2020-02-27 18:22:13 +0100

    null

--- src/app/shared/services/db-page/db-page-iterator.service.ts
+++ src/app/shared/services/db-page/db-page-iterator.service.ts
@@ -5,13 +5,13 @@ import { Component } from '@angular/core';
 @Component({})
 export class DbPageIterator<T> {
     private page: DbPage<T>;
-    private tasks: any[] = [];
+    private task: DbPageIterator.Task;
 
     constructor(private retrieveData: DbPageFetchService<T>,
         private onErrorContinue: boolean = false
-        ) {
+    ) {
         const emptyTask = (async () => { })();
-        this.tasks.push(emptyTask);
+        this.task = emptyTask;
     }
 
     //load only one page on url
@@ -25,28 +25,34 @@ export class DbPageIterator<T> {
     //load all pages on source url
     loadAll(url: string, callback?: Function, errorCallback?: Function): this {
         this.addSyncTask(async () => {
-            const it = new DbPageIterator<T>(this.retrieveData);
+            
+            this.init(url, async () => {
 
-            it.init(url, callback, errorCallback);
-            this.page = {
-                'count': it.page.count,
-                'next': it.page.next,
-                'previous': it.page.previous,
-                'results': []
-            };
-            const start = it.results;
+                    const start = this.page.results;
 
-            const prev = [], next = [];
-            while (it.hasNext()) {
-                it.loadNext(() => next.push(...it.results), 1, errorCallback);
-            }
+                    this.page = {
+                        'count': 0,
+                        'next': null,
+                        'previous': null,
+                        'results': []
+                    };
+                    
+                    const prev = [], next = [];
 
-            it.init(url, callback, errorCallback);
-            while (it.hasPrevious()) {
-                it.loadPrevious(() => prev.push(...it.results), 1, errorCallback);
-            }
+                    while (await this.hasNext()) {
+                        this.loadNext(() => next.push(...this.results), 1, errorCallback);
+                    }
 
-            this.page.results = [...prev, ...start, ...next];
+                    this.init(url, undefined, errorCallback);
+                    while (await this.hasPrevious()) {
+                        this.loadPrevious(() => prev.push(...this.results), 1, errorCallback);
+                    }
+
+                    this.page.results = [...prev, ...start, ...next];
+                    this.page.count = this.page.results.length;
+                    console.log(`${prev}, ${start} ${next}`);
+                    await callback();
+                }, errorCallback);
         });
         return this;
     }
@@ -56,7 +62,7 @@ export class DbPageIterator<T> {
             let offsetArg: number = offset === undefined ? 1 : offset;
 
             for (let i = 0; i < offsetArg; i++) {
-                if (this.hasNext()) {
+                if (await this.hasNext()) {
                     if (i + 1 === offset) {
                         await this.download(this.page.next, callback, errorCallback);
                     } else {
@@ -75,7 +81,7 @@ export class DbPageIterator<T> {
             let offsetArg: number = offset === undefined ? 1 : offset;
 
             for (let i = 0; i < offsetArg; i++) {
-                if (this.hasNext()) {
+                if (await this.hasNext()) {
                     if (i + 1 === offset) {
                         await this.download(this.page.previous, callback, errorCallback);
                     } else {
@@ -89,12 +95,18 @@ export class DbPageIterator<T> {
         return this;
     }
 
-    hasNext(): Boolean {
-        return this.page.next !== null;
+    hasNext(): Promise<boolean> {
+        this.addSyncTask((async () => {
+            return this.page.next !== null;
+        }))
+        return this.task as Promise<boolean>;
     }
 
-    hasPrevious(): Boolean {
-        return this.page.previous !== null;
+    hasPrevious(): Promise<Boolean> {
+        this.addSyncTask((async () => {
+            return this.page.previous !== null;
+        }))
+        return this.task as Promise<boolean>;
     }
 
     get results(): T[] {
@@ -102,23 +114,28 @@ export class DbPageIterator<T> {
     }
 
     private addSyncTask(callback: Function): void {
-        this.tasks.push((async () => {
-            const last = this.tasks.length - 1;
-            await this.tasks[last];
+        (this.task as any).then((async () => {
+            await this.task;
             await callback();
         })());
     }
 
-    private async download(url: string, callback: Function, errorCallback?: Function): Promise<void> {
+    private async download(url: string, callback?: Function, errorCallback?: Function): Promise<void> {
         return this.retrieveData.fetch(url)
-        .catch(reason => {if(errorCallback !== undefined) errorCallback(reason)})
-        .then((data: DbPage<T>) => {
-            if(data === undefined && this.onErrorContinue) {
-                return;
-            } else {
-                this.page = data;
-                callback();
-            }
-        });
+            .catch(reason => { if (errorCallback !== undefined) errorCallback(reason) })
+            .then((data: DbPage<T>) => {
+                if (data === undefined && this.onErrorContinue) {
+                    return;
+                } else {
+                    this.page = data;
+                    if (callback !== undefined) {
+                        callback();
+                    }
+                }
+            });
     }
+}
+
+namespace DbPageIterator {
+    export type Task = Promise<any> | ((value: any) => any);
 }
