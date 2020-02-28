ac3d9229cb0f00d4bb1d43b94eee1928ade258c5
dokolyski <dominikkalinowski15@wp.pl>
2020-02-28 10:56:17 +0100

    Merge branch 'master' of https://github.com/DamianTab/Front-SW
    # Conflicts:
    #	src/app/shared/components/charts/chart-widget/chart-widget.component.html

--- src/app/shared/services/charts/chart.service.ts
+++ src/app/shared/services/charts/chart.service.ts
@@ -1,60 +1,89 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {RequestService} from '../request/request.service';
+import { Observable } from 'rxjs';
+import { ContainerState } from '../../models/container-state';
+import {HttpClient} from '@angular/common/http';
+import {Page} from '../../../models/page';
+
+export class ContainerRecord {
+
+  constructor(time: string, value: number) {
+    this.time = time;
+    this.value = value;
+  }
+
+  time: string;
+  value: number;
+}
 
 @Injectable()
 export class ChartService {
 
-  private readonly url = 'service/chart'
+  private result: ContainerRecord[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private reqService: RequestService) { }
 
-  async retrieveData(dataType: string): Promise<ChartService.Data> {
-    let data: ChartService.Data;
-    await this.http.get(`${this.url}/${dataType}`).subscribe((recv: ChartService.Data) => data = recv);
-    return data;
+  getData(interval: ChartService.MetaData): Observable<any> {
+    return new Observable(subscriber => {
+    this.reqService.getStates(`/water/1/container/1/states/`, 10, null)
+        .subscribe((data: any[]) => {
+          const result: ContainerRecord[] = [];
+          data.forEach(elem => {
+            let date = new Date(elem.time);
+
+            result.push(new ContainerRecord(`${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}.${date.getUTCMilliseconds()}`);
+          ));
+            result.push(`${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}.${date.getUTCMilliseconds()}`);
+
+            subscriber.next();
+                subscriber.complete();
+          });
+        });
+    });
   }
 
-  temperature(meta: ChartService.MetaData) {
+temperature(meta: ChartService.MetaData) {
     return {
-      x: [1,2,3],
-      y: [4,5,6]
-    }
-    //return this.retrieveData('temperature')
+      x: [1, 2, 3],
+      y: [4, 5, 6]
+    };
+    // return this.retrieveData('temperature')
   }
 
   oxygen(meta: ChartService.MetaData) {
-    return {
-      x: [1,2,3],
-      y: [4,5,6]
-    }
-    //return this.retrieveData('oxygen')
+    this.getData(meta).subscribe( data => data );
   }
+    // return {
+    //   x: [1, 2, 3],
+    //   y: [4, 5, 6]
+    // };
+
 
   redox(meta: ChartService.MetaData) {
     return {
-      x: [1,2,3],
-      y: [4,5,6]
-    }
-    //return this.retrieveData('redox')
+      x: [1, 2, 3],
+      y: [4, 5, 6]
+    };
+    // return this.retrieveData('redox')
   }
 
   pH(meta: ChartService.MetaData) {
     return {
-      x: [1,2,3],
-      y: [4,5,6]
-    }
-    //return this.retrieveData('pH')
+      x: [1, 2, 3],
+      y: [4, 5, 6]
+    };
+    // return this.retrieveData('pH')
   }
 }
 
 export namespace ChartService {
   export interface MetaData {
-    begin: Date,
-    end: Date,
+    begin: Date;
+    end: Date;
   }
 
   export interface Data {
-    x: number[]|string[]|Date[],
-    y: number[]
+    x: number[]|string[]|Date[];
+    y: number[];
   }
 }
