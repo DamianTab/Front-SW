82b995643ff15e4950217ca4c3f7d77df5763892
Adam Lewandowski <razzors1998@gmail.com>
2020-04-01 18:45:02 +0200

    Table lazy loading (#43)
    * Intercept reply code in acquire_manual_stering.
    
    * Fixed table lazy loading
    
    * Update table.component.ts

--- src/app/shared/services/steering-state/steering-state.service.ts
+++ src/app/shared/services/steering-state/steering-state.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { SteeringState } from '../../models/steering-state';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SteeringStateService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-
-  tryToChangesStationSteeringState(stationName: string, stationId: number, steeringState: SteeringState) {
+  public tryToChangesStationSteeringState(
+    stationName: string,
+    stationId: number,
+    steeringState: SteeringState
+  ) {
     const body = new SteeringStateDTO(steeringState);
-    return this.httpClient.post(`/${stationName}/${stationId}/states/`, body);
+    return this.httpClient
+      .post(`/${stationName}/${stationId}/states/`, body)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 304) {
+            return 'Ok';
+          } else {
+            return throwError(error);
+          }
+        })
+      );
   }
-
 }
 
 class SteeringStateDTO {
-  constructor(public steering_state: SteeringState) { }
+  /* tslint:disable-next-line */
+  constructor(public steering_state: SteeringState) {}
 }
